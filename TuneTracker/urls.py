3dc9edc2c0e08bc68c8808eb08c500b092d2813d
Pushker-stark <pushkernaresh0607@gmail.com>
2024-01-11 20:42:56 +0530

    mostly done

--- TuneTracker/urls.py
+++ TuneTracker/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from .import views
 from .views import home,artist,albums,song_upload
 
 urlpatterns = [
     path('', home, name='home'),
     # path('songs/<int:song_id>/', song_detail, name='song_detail'),
-    path('artist/', artist, name='artist'),
+    path('artist/', views.artist, name='artist'),
+    path('artist_songs/<str:artist_name>/', views.artist_song, name='artist_song'),
     path('upload/',song_upload,name='upload'),
     path('album/',albums,name='albums'),
 ]
--- TuneTracker/views.py
+++ TuneTracker/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Song,Artist
+from .forms import SongForm
 
 def home(request):
-    songs = Song.objects.all()
+    songs = Song.objects.all()[:8]
     return render(request, 'index.html', {'songs': songs})
 
 def artist(request):
@@ -10,8 +12,52 @@ def artist(request):
     artist=Artist.objects.all()
     return render(request, 'artist.html', {'artist': artist})
 
-def song_upload(request):
-    return render(request,'form.html')
+def artist_song(request,artist_name):
+
+    try:
+        artistname = Artist.objects.get(name__iexact=artist_name)
+        songs = Song.objects.filter(artist=artistname)
+    except Artist.DoesNotExist:
+        songs = []
+
+    song_cnt=songs.count()
+
+    params = {'songs': songs, 'artistname':artistname,'song_cnt':song_cnt}
+    return render(request,'artist_song.html',params)
 
 def albums(request):
-    return render(request,'album.html')
+    songs = Song.objects.all()
+    return render(request,'album.html',{'songs':songs})
+
+def song_upload(request):
+    if request.method == 'POST':
+        title=request.POST.get('title')
+        artist=request.POST.get('artist')
+        songurl=request.POST.get('songurl')
+        imageurl=request.POST.get('imageurl')
+
+        artistname=''
+        try:
+            artistname = Artist.objects.get(name__iexact=artist)
+            # Perform actions when artist is found
+        except Artist.DoesNotExist:
+            art=Artist(name=artist)
+            art.save()
+            artistname=artist
+            # Handle the case when the artist is not found
+            # You might want to return an HTTP 404 response or render an error page
+        song = Song(title=title,artist=artistname,songurl=songurl,imageurl=imageurl)
+        song.save()
+        # if form.is_valid():
+        #     form.save()
+
+        print("save the data")
+        return redirect('home')  # Redirect to a success page or another URL
+        # else:
+        #     print(form.errors)
+    else:
+        form = SongForm()
+        messages.warning(request, "No submitted!!")
+
+    return render(request, 'form.html', {'form': form})
+
