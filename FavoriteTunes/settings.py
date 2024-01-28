b61303d0170d371581ace666d4e909c0ea31ddf7
Pushker-stark <pushkernaresh0607@gmail.com>
2024-01-28 20:59:51 +0530

    ready for production mode

--- FavoriteTunes/settings.py
+++ FavoriteTunes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u5)hvcvjg=n-ucm=#@kn031!u#(3uq_+pexz2lu303+^bceytj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 
